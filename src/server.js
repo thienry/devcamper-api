14847a559393c0d27f4c616d2e61046de89c8359
Thiago Moura <thmoura14@hotmail.com>
2021-04-22 17:20:05 -0300

    feat: Added morgan on project

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import 'dotenv/config'
 import cors from 'cors'
+import morgan from 'morgan'
 import express from 'express'
+
+import environment from './constants/config'
 
 const app = express()
 const PORT = process.env.PORT || 5000
@@ -8,6 +11,10 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.disable('x-powered-by')
 
+if (process.env.NODE_ENV === environment.development) app.use(morgan())
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+app.on('error', (error) => console.error(error.message))
