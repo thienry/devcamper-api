979d472c470615fb913b53fb1106330b46dcbb54
Thiago Moura <thmoura14@hotmail.com>
2021-05-04 21:57:06 -0300

    Adding pagination to getBootcamps endpoint

--- src/controllers/bootcamps.js
+++ src/controllers/bootcamps.js
@@ -21,7 +21,7 @@ import { ErrorResponse } from '../utils/ErrorResponse'
 export const getBootcamps = asyncHandler(async (req, res) => {
   const reqQuery = { ...req.query }
 
-  const removeFields = ['select', 'sort']
+  const removeFields = ['select', 'sort', 'page', 'limit']
   removeFields.forEach((param) => delete reqQuery[param])
 
   const queryStr = JSON.stringify(reqQuery)
@@ -44,11 +44,38 @@ export const getBootcamps = asyncHandler(async (req, res) => {
     query = query.sort('-createdAt')
   }
 
+  const page = parseInt(req.query.page, 10) || 1
+  const limit = parseInt(req.query.limit, 10) || 20
+  const startIndex = (page - 1) * limit
+  const endIndex = page * limit
+  const total = await Bootcamp.countDocuments()
+
+  query = query.skip(startIndex).limit(limit)
+
   const bootcamps = await query
 
-  res
-    .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps })
+  const pagination = {}
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit
+    }
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit
+    }
+  }
+
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    pagination,
+    data: bootcamps
+  })
 })
 
 /**
