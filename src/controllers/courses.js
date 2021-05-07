d3c65b9e50842105c968b751a128b6e4bf07f7fe
Thiago Moura <thmoura14@hotmail.com>
2021-05-07 20:23:31 -0300

    Single Course Add Course

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -1,7 +1,9 @@
 /** @namespace CourseControllers */
 
 import Course from '../models/Course'
+import Bootcamp from '../models/Bootcamp'
 import asyncHandler from '../middlewares/async'
+import { ErrorResponse } from '../utils/ErrorResponse'
 
 /**
  * @async
@@ -27,4 +29,67 @@ export const getCourses = asyncHandler(async (req, res) => {
   const courses = await query
 
   res.status(200).json({ success: true, count: courses.length, data: courses })
+})
+
+/**
+ * @async
+ * @auth          Public
+ * @route         GET /api/v1/courses
+ * @author        Thiago Moura <thmoura14@gmail.com>
+ * @description   Get single course
+ * @memberof      CourseControllers
+ 
+ * @returns   {Promise<void>}
+ * @param     {Object} req - The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on
+ * @param     {Object} res - The res object represents the HTTP response that an Express app sends when it gets an HTTP request.
+ * @param     {function(Object)} next - The handler to call.
+ */
+export const getCourse = asyncHandler(async (req, res, next) => {
+  const courseId = req.params.id
+
+  const course = await Course.find(courseId).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  })
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${courseId}`), 404)
+  }
+
+  res.status(200).json({ success: true, data: course })
+})
+
+/**
+ * @async
+ * @auth          Private
+ * @route         POST /api/v1/bootcamps/:bootcampId/courses
+ * @author        Thiago Moura <thmoura14@gmail.com>
+ * @description   Create new courses
+ * @memberof      BootcampControllers
+
+ 
+ * @returns   {Promise<void>}
+ * @param     {Object} req - The req object represents the HTTP request and has properties for the request query string, parameters, body, HTTP headers, and so on
+ * @param     {Object} res - The res object represents the HTTP response that an Express app sends when it gets an HTTP request.
+ * @param     {function(Object)} next - The handler to call.
+ */
+export const createCourse = asyncHandler(async (req, res, next) => {
+  const { bootcampId } = req.params
+  req.body.bootcamp = bootcampId
+
+  const bootcamp = await Bootcamp.findById(bootcampId).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  })
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Mo bootcamp with the id of ${bootcampId}`),
+      404
+    )
+  }
+
+  const course = await Course.create(req.body)
+
+  res.status(201).json({ success: true, data: course })
 })
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -4,6 +4,11 @@ import * as courseController from '../controllers/courses'
 
 const router = Router({ mergeParams: true })
 
-router.route('/').get(courseController.getCourses)
+router
+  .route('/')
+  .get(courseController.getCourses)
+  .post(courseController.createBootcamp)
+
+router.route('/:id').get(courseController.getCourse)
 
 export default router
